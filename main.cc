df03e55669a506e0fac9c580ea85f5bc6c1359da
Ana Santos <17-10602@usb.ve>
2024-03-04 22:36:31 -0400

    Lista la implementacion de los algoritmos de busqueda

--- main.cc
+++ main.cc
@@ -39,6 +39,8 @@ class hash_table_t : public unordered_map<state_t, stored_info_t, hash_function_
 hash_table_t TTable[2];
 
 //Algoritmos de busqueda
+
+//Negamax sin poda alpha-beta
 int negamax(state_t state, int depth, int color, bool use_tt = false) {
 
     if (depth == 0 || state.terminal())
@@ -53,16 +55,91 @@ int negamax(state_t state, int depth, int color, bool use_tt = false) {
     }
 
     expanded++;
-    
     return score;
 };
 
 
 
+//Negamax con poda alpha-beta
+int negamax(state_t state, int depth, int alpha, int beta, int color, bool use_tt = false) {
+    if (depth == 0 || state.terminal())
+        return color * state.value();
+    
+    int score = 0;
+    std::vector<state_t> moves = state.get_random_move(color == 1);
 
-int negamax(state_t state, int depth, int alpha, int beta, int color, bool use_tt = false);
-int scout(state_t state, int depth, int color, bool use_tt = false);
-int negascout(state_t state, int depth, int alpha, int beta, int color, bool use_tt = false);
+    for (int m = 0; m < moves.size(); m++) {
+        generated++;
+        score = max(score, -negamax(moves[m], depth - 1, -beta, -alpha, -color));
+        alpha = max(alpha, score);
+
+        if (alpha >= beta) 
+            break;
+    }
+    expanded++;
+    return score;
+};
+
+
+//Scout
+int scout(state_t state, int depth, int color, bool use_tt = false) {
+    if (depth == 0 || state.terminal())
+        return color * state.value();
+
+    int score = 0;
+    bool is_max = color == 1;
+    bool first = true;
+    std::vector<state_t> moves = state.get_random_move(color == 1);
+
+    for (int m = 0; m < moves.size(); m++) {
+        generated++;
+        if (first) {
+            first = false;
+            score = scout(moves[m], depth - 1, -color);
+            continue;
+        }
+
+        if (is_max && (depth == 0 || moves[m].terminal() || moves[m].value() > score)) {
+            score = scout(moves[m], depth - 1, -color);
+        }
+    }
+    expanded++;
+    return score; 
+   
+};
+
+//Negascout
+int negascout(state_t state, int depth, int alpha, int beta, int color, bool use_tt = false) {
+    if (depth == 0 || state.terminal())
+        return color * state.value();
+
+    int score = 0;
+    bool first = true;
+    std::vector<state_t> moves = state.get_random_move(color == 1);
+
+    for (int m = 0; m < moves.size(); m++) {
+        generated++;
+        if (first) {
+            first = false;
+            score = -negascout(moves[m], depth - 1, -beta, -alpha, -color);
+        } else {
+            score = -negascout(moves[m], depth - 1, -alpha - 1, -alpha, -color);
+            if (alpha < score && score < beta) {
+                score = -negascout(moves[m], depth - 1, -beta, -score, -color);
+            }
+        }
+
+        alpha = max(alpha, score);
+        if (alpha >= beta) 
+            break;
+    }
+    expanded++;
+    return alpha;
+
+};
+
+
+
 
 
 //Main
@@ -90,7 +167,9 @@ int main(int argc, const char **argv) {
 #if 0
     // print principal variation
     for( int i = 0; i <= npv; ++i )
-        cout << pv[npv - i];
+        cout << pv[npv - i] << endl;
+        pv[npv - i].print_bits(cout);
+        cout << endl;
 #endif
 
     // Print name of algorithm
@@ -121,11 +200,11 @@ int main(int argc, const char **argv) {
             if( algorithm == 1 ) {
                 value = negamax(pv[i], 0, color, use_tt);
             } else if( algorithm == 2 ) {
-                //value = negamax(pv[i], 0, -200, 200, color, use_tt);
+                value = negamax(pv[i], 0, -200, 200, color, use_tt);
             } else if( algorithm == 3 ) {
-                //value = scout(pv[i], 0, color, use_tt);
+                value = scout(pv[i], 0, color, use_tt);
             } else if( algorithm == 4 ) {
-                //value = negascout(pv[i], 0, -200, 200, color, use_tt);
+                value = negascout(pv[i], 0, -200, 200, color, use_tt);
             }
         } catch( const bad_alloc &e ) {
             cout << "size TT[0]: size=" << TTable[0].size() << ", #buckets=" << TTable[0].bucket_count() << endl;
